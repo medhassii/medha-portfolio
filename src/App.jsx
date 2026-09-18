af764f70c0e22d197b2ca568ee0cd5093118b23e
Medha <medhasingh1399@gmail.com>
2026-09-18 20:42:30 +0530

    adding navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
     <div>
+      <Navbar />
+      
       <h1>Medha's Portfolio</h1>
     </div>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Medha</h2>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Experience</li>
+        <li>Projects</li>
+        <li>Skills</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
